49897e24bdbfe7d03948a7d64ef30ffd69ddc055
Sol0221 <slee505@ivc.edu>
2020-09-08 08:06:59 -0700

    countdown timer added

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -3,42 +3,31 @@
 #include <cstdlib>
 #include <ctime>
 
-#include<stdlib.h>
-
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+
+    //Welcoming The Player
     PrintLine(TEXT("Welcome to Bull Cows! "));
-    PrintLine(TEXT("Press enter to continue..."));
-    FString roc = "1) Rock\n";
-    FString pap = "2) Paper\n";
-    FString sci = "3) Scissors\n";
-    
-    PrintLine(TEXT("====================\n"));
-    PrintLine(TEXT("rock paper scissors!\n"));
-    PrintLine(TEXT("====================\n"));
-    
-    PrintLine(roc);
-    PrintLine(pap);
-    PrintLine(sci);
-    
-    PrintLine(TEXT("Choose: "));
+    SetupGame();
+
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
 
-    
     srand(time(NULL));
 
     int32 computer = rand() % 3 + 1;
 
     int32 user = 0;
-    FString roc = "1) Rock\n";
-    FString pap = "2) Paper\n";
-    FString sci = "3) Scissors\n";
-    
+    FString roc = "Computer: Rock";
+    FString pap = "Computer: Paper";
+    FString sci = "Computer: Scissors";
+
     if (Input == "rock")
     {
         user = 1;
@@ -47,53 +36,95 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
     {
         user = 2;
     }
-    else if (Input == "Scissors")
+    else if (Input == "scissors")
     {
         user = 3;
     }
     else
     {
-        PrintLine("Wrong input");
+        PrintLine("Wrong input!");
+        return;
     }
 
-    
-    switch(computer){
-    case 1 :
+
+    switch (computer)
+    {
+    case 1:
         PrintLine(roc);
         break;
-    case 2 :
+    case 2:
         PrintLine(pap);
         break;
-    case 3 :
+    case 3:
         PrintLine(sci);
         break;
-    default :
+    default:
         PrintLine(TEXT("Invalid Option\n"));
     }
-    
-    
-    
-    if(user == computer){
-       PrintLine(TEXT("Draw Game\n"));
+
+
+
+    if (user == computer)
+    {
+        PrintLine(TEXT("Draw Game"));
     }
-    else if(user == 1 && computer == 3){
-        PrintLine(TEXT("You Win\n"));
-        
+    else if (user == 1 && computer == 3)
+    {
+        PrintLine(TEXT("You Win"));
+        ++userWins;
     }
-    else if(user == 3 && computer == 2){
-        PrintLine(TEXT("You Win\n"));
-        
+    else if (user == 3 && computer == 2)
+    {
+        PrintLine(TEXT("You Win"));
+        ++userWins;
     }
-    else if(user == 2 && computer == 1){
-      PrintLine(TEXT("You Win\n"));
+    else if (user == 2 && computer == 1)
+    {
+        PrintLine(TEXT("You Win"));
+        ++userWins;
     }
-    else{
-        PrintLine(TEXT("Computer Wins!\n"));
-       
+    else
+    {
+        PrintLine(TEXT("Computer Wins!"));
+        ++computerWins;
     }
-    
-    
-    
+
+    ShowOutput();
+    UserInput();
+
 }
+
+void UBullCowCartridge::SetupGame()
+{
+    ClearScreen();
+    userWins = 0;
+    computerWins = 0;
+    UserInput();
+}
+
+void UBullCowCartridge::UserInput()
+{
+    FString roc = "1) Rock";
+    FString pap = "2) Paper";
+    FString sci = "3) Scissors";
+
+    PrintLine(TEXT("===================="));
+    PrintLine(TEXT("rock paper scissors!"));
+    PrintLine(TEXT("===================="));
+
+    PrintLine(roc);
+    PrintLine(pap);
+    PrintLine(sci);
+
+    PrintLine(TEXT("Choose: "));
+}
+
+void UBullCowCartridge::ShowOutput()
+{
+    PrintLine(TEXT("Computer : You"));
+    PrintLine(TEXT("%i : %i"), computerWins, userWins);
+    return;
+}
+
 
 
--- Source/BullCowGame/BullCowCartridge.h
+++ Source/BullCowGame/BullCowCartridge.h
@@ -7,17 +7,27 @@
 #include "Console/Cartridge.h"
 #include "BullCowCartridge.generated.h"
 
-UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+
+
+UCLASS(Blueprintable)
 class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 {
     GENERATED_BODY()
 
-    public:
+public:
     virtual void BeginPlay() override;
     virtual void OnInput(const FString& Input) override;
+    void SetupGame();
+    void UserInput();
+    void ShowOutput();
+
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "TestCategory")
+        int32 userWins = 0;
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "TestCategory")
+        int32 computerWins = 0;
 
     // Your declarations go below!
-    private:
+private:
     int32 computer;
-    
 };
